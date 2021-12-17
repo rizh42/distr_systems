606d909bc122d93fd71b7814f69ae226b22e15c2
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 02:59:53 +0300

    Scripted auto-commit on change (2021-12-18 02:59:53) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirMapper.java
+++ lab2/src/main/java/bmstu/iu9/AirMapper.java
@@ -2,7 +2,7 @@ package bmstu.iu9;
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop..Text;
+import org.apache.hadoop.mapreduce.Mapper;
 
 
 public class AirMapper extends Mapper{
