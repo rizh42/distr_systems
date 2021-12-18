6b9dc706dcb22a2ddaa2d0debf2cd9fd40a4fe3f
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:02:47 +0300

    Scripted auto-commit on change (2021-12-18 03:02:47) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirMapper.java
+++ lab2/src/main/java/bmstu/iu9/AirMapper.java
@@ -10,6 +10,6 @@ public class AirMapper extends Mapper<LongWritable, Text>{
 
     @Override
     protected void map(LongWritable key, Text value){
-        String[] params = value.toString().
+        String[] params = value.toString().split()
     }
 }
