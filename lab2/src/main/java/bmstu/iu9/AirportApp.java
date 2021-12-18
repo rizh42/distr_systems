9f25652c6ffdf0769c331e56008bd912ae0ecdb5
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 04:04:17 +0300

    Scripted auto-commit on change (2021-12-18 04:04:17) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportApp.java
+++ lab2/src/main/java/bmstu/iu9/AirportApp.java
@@ -23,6 +23,6 @@ public class AirportApp {
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
         job,setReducerClass(AirportReduce.class);
         job.setOutputKeyClass(Text.class);
-        job.
+        job.setOutputValueClass()
     }
 }
