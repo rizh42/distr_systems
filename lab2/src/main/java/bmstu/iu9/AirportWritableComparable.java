74c225cc41da156d95e18fd8ede0ed9ea7d4dfcc
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:19:05 +0300

    Scripted auto-commit on change (2021-12-18 03:19:05) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportWritableComparable.java
+++ lab2/src/main/java/bmstu/iu9/AirportWritableComparable.java
@@ -45,6 +45,6 @@ public class AirportWritableComparable implements WritableComparable {
 
     @Override
     public String toString(){
-        return "AirportWritable"+
+        return "AirportWritable(" + 
     }
 }
