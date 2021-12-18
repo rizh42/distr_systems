08e2c2c2c650ebd9209bd92640bb60b0ec5b9af3
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 09:30:01 +0300

    Scripted auto-commit on change (2021-12-18 09:30:01) by gitwatch.sh

--- lab3/src/main/java/bmstu/iu9/Flight.java
+++ lab3/src/main/java/bmstu/iu9/Flight.java
@@ -5,5 +5,6 @@ import java.io.Serializable;
 public class Flight implements Serializable {
     public int startAirportId;
     public int destAirportId;
-    
+    public float delayed;
+    public 
 }
