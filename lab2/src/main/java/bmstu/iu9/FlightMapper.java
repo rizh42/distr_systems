6716008459c69dc9e5648e70a117f503311e90aa
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:48:01 +0300

    Scripted auto-commit on change (2021-12-18 03:48:01) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/FlightMapper.java
+++ lab2/src/main/java/bmstu/iu9/FlightMapper.java
@@ -2,5 +2,7 @@ package bmstu.iu9;
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
     @Override
-    protected ciod map(LongWritable key, Text value, )
+    protected ciod map(LongWritable key, Text value, Context context){
+        
+    }
 }
