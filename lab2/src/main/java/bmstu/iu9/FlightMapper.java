dc0f050a33a2fcab7cddfb93ccf0ed968169f819
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:53:23 +0300

    Scripted auto-commit on change (2021-12-18 03:53:23) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/FlightMapper.java
+++ lab2/src/main/java/bmstu/iu9/FlightMapper.java
@@ -5,6 +5,7 @@ package bmstu.iu9;
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
 
     public static String SEP = ",";
+    public  static int 
 
     @Override
     protected ciod map(LongWritable key, Text value, Context context){
@@ -15,7 +16,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
             float delayFl = delay.isEmpty() ? 0.0f : Float.parseFloat(delay);
             if (delayFl > 0.0f){
                 int airID = Integer.parseInt(params[14]);
-                context.write(new AirportWritableComparable(airID, 1), new Text )
+                context.write(new AirportWritableComparable(airID, 1), new Text(delay))
             }
         }
     }
