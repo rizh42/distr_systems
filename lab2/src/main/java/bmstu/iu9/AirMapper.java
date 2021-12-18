4b7f69f20a606febe90f0fb3201398b8f320fa6e
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:07:53 +0300

    Scripted auto-commit on change (2021-12-18 03:07:53) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirMapper.java
+++ lab2/src/main/java/bmstu/iu9/AirMapper.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 
-public class AirMapper extends Mapper<LongWritable, Text, wtitable>{
+public class AirMapper extends Mapper<LongWritable, Text, wtitableComparable, >{
     public statit int air_id = 0;
 
     @Override
