d416f8e168acb75d07b99f0089e863f212c3e991
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:52:29 +0300

    Scripted auto-commit on change (2021-12-18 03:52:29) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/FlightMapper.java
+++ lab2/src/main/java/bmstu/iu9/FlightMapper.java
@@ -14,7 +14,7 @@ public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComp
             String delay =  params[18];
             float delayFl = delay.isEmpty() ? 0.0f : Float.parseFloat(delay);
             if (delayFl > 0.0f){
-                int airID = Integer.parseInt()
+                int airID = Integer.parseInt(params[14])
             }
         }
     }
