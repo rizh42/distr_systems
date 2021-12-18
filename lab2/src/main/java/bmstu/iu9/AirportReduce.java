53c990f0ba2fbf0bec8201163cef6825f525d68b
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:29:21 +0300

    Scripted auto-commit on change (2021-12-18 03:29:21) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportReduce.java
+++ lab2/src/main/java/bmstu/iu9/AirportReduce.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class AirportReduce extends Reducer<AirportWritableComparable, Text, Text, Text>{
     @Override
-    protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws IOException,  {
+    protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 
     }
 }
