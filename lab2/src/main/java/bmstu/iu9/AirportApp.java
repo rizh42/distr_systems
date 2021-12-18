9c6ccaa4ce9ccc949fe632c0ff98cf4fdb672329
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:57:27 +0300

    Scripted auto-commit on change (2021-12-18 03:57:27) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportApp.java
+++ lab2/src/main/java/bmstu/iu9/AirportApp.java
@@ -1,9 +1,9 @@
 package bmstu.iu9;
 
 public class AirportApp {
-    public static void main(String[] args) throws  {
+    public static void main(String[] args) throws Exception {
         if (args.length != 3){
-
+            
         }
     }
 }
