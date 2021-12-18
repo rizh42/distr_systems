087086371e8d243fc8e0daf92c61ae3a89e954c2
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:34:33 +0300

    Scripted auto-commit on change (2021-12-18 03:34:33) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportReduce.java
+++ lab2/src/main/java/bmstu/iu9/AirportReduce.java
@@ -29,7 +29,8 @@ public class AirportReduce extends Reducer<AirportWritableComparable, Text, Text
         }
 
         if (count > 0){
-            context.write(new Text("Airport name:" + air))
+            context.write(new Text("Airport name:" + air),
+                            new Text())
         }
     }
 }
