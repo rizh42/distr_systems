eaf52b9e1f79034e126f1e609e5809e836311232
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:28:51 +0300

    Scripted auto-commit on change (2021-12-18 03:28:51) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportReduce.java
+++ lab2/src/main/java/bmstu/iu9/AirportReduce.java
@@ -5,5 +5,7 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 public class AirportReduce extends Reducer<AirportWritableComparable, Text, Text, Text>{
     @Override
-    protected void reduce(AirportWritableComparable key, Iterable<Text> values, )
+    protected void reduce(AirportWritableComparable key, Iterable<Text> values, Context context){
+        
+    }
 }
