30b1bd8efa99654f501854492700884cb7e4b634
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:55:29 +0300

    Scripted auto-commit on change (2021-12-18 03:55:29) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/FlightMapper.java
+++ lab2/src/main/java/bmstu/iu9/FlightMapper.java
@@ -1,6 +1,8 @@
 package bmstu.iu9;
 
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+
 
 
 public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
