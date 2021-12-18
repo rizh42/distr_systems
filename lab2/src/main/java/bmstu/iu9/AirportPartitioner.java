21715ab43a668a56fa12ba05ecdd8ace8b88e978
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:45:04 +0300

    Scripted auto-commit on change (2021-12-18 03:45:04) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportPartitioner.java
+++ lab2/src/main/java/bmstu/iu9/AirportPartitioner.java
@@ -3,6 +3,6 @@ package bmstu.iu9;
 public class AirportPartitioner extends Partitioner<AirportWritableComparable, Text>{
     @Override
     public int getPartition(AirportWritableComparable key, Text value, int numReduceTasks){
-        return key.
+        return key.getAirportID().
     }
 }
