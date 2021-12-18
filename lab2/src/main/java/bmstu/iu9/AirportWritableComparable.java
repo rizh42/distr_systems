f4bb06479f92f16acc12746380e325f1c52d22f4
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:12:39 +0300

    Scripted auto-commit on change (2021-12-18 03:12:39) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportWritableComparable.java
+++ lab2/src/main/java/bmstu/iu9/AirportWritableComparable.java
@@ -3,6 +3,6 @@ package bmstu.iu9;
 import org.apache.hadoop.io.WritableComparable;
 
 public class AirportWritableComparable implements WritableComparable {
-    pulic AirWritableComparable();
+    pulic AirWritableComparable(){}
     
 }
