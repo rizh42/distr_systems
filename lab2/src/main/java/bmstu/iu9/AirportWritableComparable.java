4041e2291ab037320769e8fabd3bf7a268d41acd
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:13:12 +0300

    Scripted auto-commit on change (2021-12-18 03:13:12) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportWritableComparable.java
+++ lab2/src/main/java/bmstu/iu9/AirportWritableComparable.java
@@ -4,7 +4,7 @@ import org.apache.hadoop.io.WritableComparable;
 
 public class AirportWritableComparable implements WritableComparable {
     private int airportID;
-    
+    private int 
 
     pulic AirWritableComparable(){}
 
