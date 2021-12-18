fd20c0328c71a8fcf3e8af4014ee23aae8719d7b
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:47:15 +0300

    Scripted auto-commit on change (2021-12-18 03:47:15) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/FlightMapper.java
+++ lab2/src/main/java/bmstu/iu9/FlightMapper.java
@@ -1,4 +1,5 @@
 package bmstu.iu9;
 
-public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, > {
+public class FlightMapper extends Mapper<LongWritable, Text, AirportWritableComparable, Text> {
+    
 }
