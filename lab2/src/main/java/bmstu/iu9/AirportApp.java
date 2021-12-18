9874a74627c73c3890e1f975fb63be7573acd308
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:58:39 +0300

    Scripted auto-commit on change (2021-12-18 03:58:39) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportApp.java
+++ lab2/src/main/java/bmstu/iu9/AirportApp.java
@@ -4,7 +4,9 @@ public class AirportApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 3){
             System.err.println("Usage: AirportApp <flight data> <airport data> <output>");
-            System.
+            System.exit(-1);
         }
+
+        
     }
 }
