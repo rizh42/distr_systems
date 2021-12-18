c593677dd81705db123922ce577412feaedd18b2
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:27:38 +0300

    Scripted auto-commit on change (2021-12-18 03:27:38) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportReduce.java
+++ lab2/src/main/java/bmstu/iu9/AirportReduce.java
@@ -1,7 +1,7 @@
 package bmstu.iu9;
 
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.
+import org.apache.hadoop.mapreduce.Reducer;
 
-public class AirportReduce {
+public class AirportReduce  {
 }
