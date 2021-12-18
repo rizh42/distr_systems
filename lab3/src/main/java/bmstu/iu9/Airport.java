f4654af3ddacd4ec8013379a5b0b6147e0c881c6
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 09:53:29 +0300

    Scripted auto-commit on change (2021-12-18 09:53:29) by gitwatch.sh

--- lab3/src/main/java/bmstu/iu9/Airport.java
+++ lab3/src/main/java/bmstu/iu9/Airport.java
@@ -12,7 +12,7 @@ public class Airport implements Serializable {
     }
 
     public static Airport getData(String CSV){
-        String[] list = CSV.split(",", 2);
+        String[] list = CSV.replace().split(",", 2);
         return new Airport(
                 Integer.parseInt(list[0])
         );
