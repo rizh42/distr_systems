d4dde8b897dd40d27f3f1d2a441fbc7cdc780545
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 04:03:24 +0300

    Scripted auto-commit on change (2021-12-18 04:03:24) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportApp.java
+++ lab2/src/main/java/bmstu/iu9/AirportApp.java
@@ -20,6 +20,6 @@ public class AirportApp {
         job.setPartitionerClass(AirportPartitioner.class);
         job.setGroupingComparatorClass(AirportComparator.class);
 
-        FileOutputFormat.setOutputPath(job, new )
+        FileOutputFormat.setOutputPath(job, new Path(args[]))
     }
 }
