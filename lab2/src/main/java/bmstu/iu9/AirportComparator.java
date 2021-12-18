d7b7bf5b6317257300c865ce608a06d8b47e71b3
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 03:37:06 +0300

    Scripted auto-commit on change (2021-12-18 03:37:06) by gitwatch.sh

--- lab2/src/main/java/bmstu/iu9/AirportComparator.java
+++ lab2/src/main/java/bmstu/iu9/AirportComparator.java
@@ -4,5 +4,7 @@ import org.apache.hadoop.io.WritableComparable;
 import org.apache.hadoop.io.WritableComparator;
 
 public class AirportComparator extends WritableComparator {
-    protected AirportComparator()
+    protected AirportComparator(){
+        
+    }
 }
