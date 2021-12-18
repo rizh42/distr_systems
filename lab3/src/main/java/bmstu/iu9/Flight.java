f0cda9ad1790cd681fd0934209095448da68a4ac
Egor Ivanov <egorvlz2013@yandex.ru>
2021-12-18 09:30:51 +0300

    Scripted auto-commit on change (2021-12-18 09:30:51) by gitwatch.sh

--- lab3/src/main/java/bmstu/iu9/Flight.java
+++ lab3/src/main/java/bmstu/iu9/Flight.java
@@ -9,5 +9,5 @@ public class Flight implements Serializable {
     public boolean cancelled;
 
     public static int START_AIRPORT_ID_IND = 11;
-    public static int 
+    public static int DES_AIRPORT_ID_IND = 
 }
